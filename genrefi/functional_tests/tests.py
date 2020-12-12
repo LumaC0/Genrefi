657d3aeb84d72e19fc506733b0f7f8b22823c96d
mushinMiscellanea <finsp01@gmail.com>
2020-12-12 14:19:18 -0600

    first functional test created and ready to start developing

---
+++ genrefi/functional_tests/tests.py
@@ -0,0 +1,28 @@
+import time
+import unittest
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 3
+
+class GenrelUserTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_can_login_using_OAuth_spotify_client(self):
+        # Our user Chi Chi hears about a cool webapp that will give her 
+        # genre information regarding her user Library
+        # In short, she will access the site
+        self.browser.get(self.live_server_url)
+        self.assertIn('Genrefi', self.browser.title)
+        # the option to login will be visible to her
+        self.browser.find_element_by_id('login').click()
+        # she'll log into her spotify account using OAuth
+        # and the spotify client
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Successfully Logged In', text)
--- genrefi/genre_classify/tests.py
+++ genrefi/genre_classify/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+class smokeTest(TestCase):
+    def test_working_tests(self):
+        self.assertEqual(1+1,3)
