593182a5140d714f74b0abcffb96867274edd061
mushinMiscellanea <finsp01@gmail.com>
2020-12-23 12:01:36 -0600

    home view is still not redirecting the way i'd like but will do for now

--- genrefi/figenre/views.py
+++ genrefi/figenre/views.py
@@ -13,35 +13,23 @@ dotenv.read_dotenv('/home/spencer/prod/genrefi/genrefi/.env')
 CACHES_FOLDER = settings.SESSION_FILE_PATH
 
 def home(request):
+    if not request.session._session_key:
+        request.session.create()
+
     scope = 'user-library-read'
     auth_manager = spotipy.oauth2.SpotifyOAuth(
-        scope=scope,
-        cache_path=CACHES_FOLDER,
-        show_dialog=True,
+            scope=scope,
+            cache_path=CACHES_FOLDER + '/' + request.session._session_key,
+            show_dialog=True,
     )
-    print('here', request.session.keys())
+    request.session['code'] = auth_manager.get_authorization_code()
+    if (t := request.session.get('code')):
+        auth_manager.get_access_token(t)
+    print('here', request.session._session_key)
     if not auth_manager.get_cached_token():
         authUrl = auth_manager.get_authorize_url()
         return render(request, 'login.html', {'auth_url': authUrl})
     
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    #request.session['token'] = spotify._auth_headers['Authorization']
-    return render(request, 'index.html', {'token_' : request.session['token']})
-
-'''
-def login(request):
-    return HttpResponse(render(request,'login.html'))
-
-def auth(request):
-    auth_code = auth_manager.get_authorization_code()
-    if auth_code:
-        auth_manager.get_access_token(auth_code)
-        return redirect('home')
-    else:
-        auth_url = auth_manager.get_authorize_url()
-        return HttpResponseRedirect(auth_url)
-    sp = spotipy.Spotify(auth_code)
-    request.session['token'] = sp._auth_headers()['Authorization']
-    return redirect('home')#, {'auth': sp})
-'''
-    
+    request.session['token'] = spotify._auth_headers()['Authorization']
+    return render(request, 'index.html', {'token_' : request.session['code']})
