aa1a6503d27579e24c6f9db04de52e4d627c4348
mushinMiscellanea <finsp01@gmail.com>
2020-12-27 01:14:36 -0600

    Session management is in order; testing 3rd party login is problematic

--- genrefi/figenre/views.py
+++ genrefi/figenre/views.py
@@ -11,37 +11,32 @@ import dotenv
 from figenre.logic import genrefi_logic
 
 dotenv.read_dotenv('/home/spencer/prod/genrefi/genrefi/.env')
-CACHES_FOLDER = settings.SESSION_FILE_PATH
+CACHE_FILE = settings.SESSION_FILE_PATH
 
 class HomeView(View):
     scope = 'user-library-read'
-    auth_manager = None 
-                                            
-    def get(self, request):
-        if not request.session.get('access_token'):
-            return self.get_client(request)
-        #spotify = spotipy.Spotify(auth_manager=self.auth_manager)
-        #request.session['token'] = spotify._auth_headers()['Authorization']
-        return redirect('/discover')
-
-
-    def get_client(self, request):
-        request.session.flush()
-        self.auth_manager = spotipy.oauth2.SpotifyOAuth(
-                                                scope=self.scope,
-                                                cache_path=CACHES_FOLDER,
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+                                                scope=scope,
+                                                cache_path=CACHE_FILE,
                                                 show_dialog=True,
                                                 )
-        
-        request.session['access_token'] = self.auth_manager.get_access_token()['access_token']
-        request.session['token_type'] = self.auth_manager.get_access_token()['token_type']
-        request.session['auth_token'] = request.session.get('token_type') + ' ' + request.session.get('access_token') 
-        try:
-            os.rmdir(CACHES_FOLDER)
-        except NotADirectoryError:
-            pass
+                                            
+    def get(self, request):
+        if request.GET.get('code'):
+            self.auth_manager.get_access_token(request.GET.get('code'))
+            self.set_session_variables(request)
+            return redirect('/discover')
         auth_url = self.auth_manager.get_authorize_url()
         return render(request, 'login.html', {'auth_url': auth_url})
+        
+    def set_session_variables(self, request):
+        access_token = self.auth_manager.get_access_token()['access_token']
+        token_type = self.auth_manager.get_access_token()['token_type']
+        request.session['auth_token'] = token_type + ' ' + access_token
+        try:
+            os.remove(CACHE_FILE)
+        except NotADirectoryError as err:
+            print(err)
 
 
 class Discover(View):
@@ -53,4 +48,6 @@ class Discover(View):
             request.session['library_data'] = genrefi_logic.genre_fi(request.session.get('auth_token'))
         data = request.session.get('library_data')
         return render(request, 'index.html', {'sorted': data})
+
+        
 
--- genrefi/functional_tests/tests.py
+++ genrefi/functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 import unittest
 from django.test import LiveServerTestCase
@@ -5,23 +6,59 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
+import dotenv
+dotenv.read_dotenv('/home/spencer/prod/genrefi/genrefi/.env')
+
 MAX_WAIT = 3
 
 class GenrelUserTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        if (url := os.environ.get('DJANGO_LIVE_SERVER_ADDRESS')):
+            self.live_server_url = url 
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_user_can_login_using_OAuth_spotify_client(self):
+    def test_user_can_login_using_OAuth_spotify_client_and_get_data(self):
         # Our user Chi Chi hears about a cool webapp that will give her 
         # genre information regarding her user Library
         # In short, she will access the site
         self.browser.get(self.live_server_url)
+        #self.assertEqual('Poo', self.browser.current_url)
+        login_url = self.browser.current_url
         # the option to login will be visible to her
-        self.browser.find_element_by_id('Spotify').click()
+        self.browser.find_element_by_id('login').click()
+        time.sleep(1)
         # she'll log into her spotify account using OAuth
         # and the spotify client
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn('Successfully Logged In', page_text)
+        self.assertNotEqual(login_url, self.browser.current_url)
+        self.browser.find_element_by_id('login-username').send_keys('1243046973')
+        self.browser.find_element_by_id('login-password').send_keys('t659AA1qa!QA')
+        self.browser.find_element_by_id('login-button').click()
+        time.sleep(1)
+        # accept scope
+        self.browser.find_element_by_id('auth-accept').click()
+        time.sleep(1)
+        # Chi Chi clicks on 'discover' to access her library statistics
+        
+        self.browser.find_element_by_id("press-for-magic").click()
+        # now we check how long it takes to get her info
+        # and if she even gets her info
+        populated_table = None
+        for i in range(10):
+            try:
+                populated_table = self.browser.find_element_by_id('genres')
+                break
+            except:
+                time.sleep(1)
+                continue
+            raise(TimeoutError)
+    
+        rows = populated_table.find_element_by_tag_name('tr')
+        self.assertIn('drum and bass', [row.text for row in rows])
+        
+
+    
+    def test_multiuser_login_and_use(self):
+        ...
